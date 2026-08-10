81a538d73200ee122c054e02dd43dd1e6bc4cd42
Swift <s87217647@gmail.com>
2026-08-10 16:52:58 -0700

    cpp file

---
+++ renderer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+main() {
+    // parsing files - getting vertex and surface
+    ifstream file("pyramid.obj");
+
+    string line;
+    while (getline(file, line)) {
+        if line[0]
+
+    }
+
+
+
+    cout << "Hellor, world!" << endl;
+    return 0;
+
+    
+}
