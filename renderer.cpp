672ddfda41100a224c1188f2322bdda293146243
Swift <s8721747@gmail.com>
2026-08-11 00:25:01 -0700

    load vertices and surfaces

--- renderer.cpp
+++ renderer.cpp
@@ -1,22 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
-main() {
+struct Vec3 {
+    float x, y, z;
+};
+
+struct Surface {
+    int v1, v2, v3;
+};
+
+int main(){
     // parsing files - getting vertex and surface
     ifstream file("pyramid.obj");
-
     string line;
-    while (getline(file, line)) {
-        if line[0]
 
+    vector<Vec3> vertices;
+    vector<Surface> surfaces;
+    
+    vertices.push_back(Vec3{0.0f, 0.0f, 0.0f}); // Sentinal 0 idx
+     
+    
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string prefix;
+        ss >> prefix;
+        if (prefix == "v") {
+            Vec3 vertex;
+            ss >> vertex.x >> vertex.y >> vertex.z;
+            vertices.push_back(vertex);
+        } else if (prefix == "f") {
+            Surface surface;
+            ss >> surface.v1 >> surface.v2 >> surface.v3;
+            surfaces.push_back(surface);
+        }    
     }
 
+    
 
-
-    cout << "Hellor, world!" << endl;
+    // Guess this is my world now
+    cout << "This is the end, hold your breath and count to ten" << endl;
     return 0;
 
     
